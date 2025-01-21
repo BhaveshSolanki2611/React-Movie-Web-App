defdbd4942bd5d5c346b8fc9685d93b8a4c42a86
Bhavesh Solanki <bhaveshsolanki26112004@gmail.com>
2025-01-22 04:45:23 +0530

    First commit
    A fast and responsive movie search platform built with React.js and Vite, featuring dynamic movie details, search functionality, and a sleek UI. 🚀

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "82255380ed4a997e41c32f4cf297c9af";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
